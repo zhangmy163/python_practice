661af644359f937d78b20d17a6a85b18aabf017a
zhangmingyuan <zhangmingyuan1@cig.com.cn>
2020-10-20 14:26:47 +0800

    update testjson

--- json_practice/test.py
+++ json_practice/test.py
@@ -14,36 +14,60 @@ def existjson(s,js):
         return(formatstr("is not exist"))
 
 
-# message,script,line,error,url,ua,other
-with open(str(base_dir)+"/file/error.csv",'a') as wf:
-    wf.writelines("message"+","+"script"+","+"line"+","+"error"+","+"url"+","+"other"+","+"ua"+"\n")
+# message,script,line,error,url,other,ua,
+with open(str(base_dir)+"/file/error1.csv",'a') as wf:
+    wf.writelines("type"+","+"message"+","+"script"+","+"line"+","+"error"+","+"url"+","+"other"+","+"ua"+"\n")
+with open(str(base_dir)+"/file/error2.csv",'a') as wf:
+    wf.writelines("type"+","+"info"+","+"url"+","+"stack"+","+"other"+","+"ua"+"\n")
 with open(str(base_dir)+"/file/error.log",'rb') as f:
     for line in f:
         l=[]
         data=json.loads(line)
         # print(line)
         # print(data["info"])
-
-        try:
-            datainfo=json.loads(data["info"])
-        except:
-            msg=formatstr("info is not json")
-            scr='\"\"'
-            li='\"\"'
-            err='\"\"'
-            url='\"\"'
-            other=formatstr(data["info"]).replace(",",";")
-            with open(str(base_dir)+"/file/error.csv",'a') as wf:
-                wf.writelines(msg+","+scr+","+li+","+err+","+url+","+other)
-        else:
-            msg=existjson("message",datainfo)
-            scr=existjson("script",datainfo)
-            li=existjson("line",datainfo)
-            err=existjson("error",datainfo)
-            url=existjson("url",datainfo)
-            other='\"\"'
-            with open(str(base_dir)+"/file/error.csv",'a') as wf:
-                wf.writelines(msg+","+scr+","+li+","+err+","+url+","+other)
-        with open(str(base_dir)+"/file/error.csv",'a') as wf:
-                wf.writelines(","+formatstr(data["ua"])+"\n")
-
+        lv=data["level"]
+        tp=data["msg"]
+        if(lv == "info"):
+            try:
+                datainfo=json.loads(data["info"])
+            except:
+                msg=formatstr("info is not json")
+                scr='\"\"'
+                li='\"\"'
+                err='\"\"'
+                url='\"\"'
+                other=formatstr(data["info"].replace('"',"'"))
+                with open(str(base_dir)+"/file/error1.csv",'a') as wf:
+                    wf.writelines(tp+","+msg+","+scr+","+li+","+err+","+url+","+other)
+            else:
+                msg=existjson("message",datainfo)
+                scr=existjson("script",datainfo)
+                li=existjson("line",datainfo)
+                err=existjson("error",datainfo)
+                url=existjson("url",datainfo)
+                other='\"\"'
+                with open(str(base_dir)+"/file/error1.csv",'a') as wf:
+                    wf.writelines(tp+","+msg+","+scr+","+li+","+err+","+url+","+other)
+            with open(str(base_dir)+"/file/error1.csv",'a') as wf:
+                    wf.writelines(","+formatstr(data["ua"])+"\n")
+        if(lv == "warn"):
+            try:
+                databody=json.loads(data["body"])
+            except:
+                # "info"+","+"url"+","+"stack"+","+"ua"+other
+                info=formatstr("body is not json")
+                url='\"\"'
+                stack='\"\"'
+                other=formatstr(data["body"].replace('"',"'"))
+                ua=formatstr(existjson("ua",databody))
+                with open(str(base_dir)+"/file/error2.csv",'a') as wf:
+                    wf.writelines(tp+","+info+","+url+","+stack+","+other+","+ua+"\n")
+            else:
+                info=existjson("info",databody)
+                url=existjson("url",databody)
+                stack=existjson("stack",databody)
+                ua=formatstr(existjson("ua",databody))
+                other='\"\"'
+                with open(str(base_dir)+"/file/error2.csv",'a') as wf:
+                    wf.writelines(tp+","+info+","+url+","+stack+","+other+","+ua+"\n")
+            
